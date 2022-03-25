f7d8a805f881c9d0b30cf0ddc97fe9377d0b3cb0
Joltra <MinnyMouseXDmail.com>
2022-03-25 19:10:29 +0100

    Add color parameter

--- Room.py
+++ Room.py
@@ -1,16 +1,17 @@
 import pygame
-from Pygame.Snake.Enums import Color
+from Globals import Color
 import Globals
 
 
 class Room:
-    def __init__(self, x: int, y: int):
+    def __init__(self, x: int, y: int, color: Color):
         self.__x: int = x
         self.__y: int = y
-        self.__rect = pygame.Rect(self.__x * Globals.width, self.__y * Globals.height, Globals.width, Globals.height)
+        self.__color = color
+        self.__rect = pygame.Rect(self.__x * Globals.room_width + Globals.floor_plan_coordinates[1], self.__y * Globals.room_height + Globals.floor_plan_coordinates[0], Globals.room_width, Globals.room_height)
 
     def draw(self, screen):
-        pygame.draw.rect(screen, Color.VIOLET.value, self.__rect, 2)
+        pygame.draw.rect(screen, self.__color.value, self.__rect)
 
     def get_x(self):
         return self.__x
