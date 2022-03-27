22a8b97d3c5972e70d935279e2a34e1db433758f
Joltra <MinnyMouseXDmail.com>
2022-03-27 19:18:29 +0200

    Added queue

--- Generator.py
+++ Generator.py
@@ -1,13 +1,10 @@
 import math
 import random
-from enum import Enum
-
-import numpy as np
-
 import Globals
 from Globals import Color
 from Floor import Floor
 import pygame
+from collections import deque
 
 number_of_rooms: int
 max_rooms: int = 15
@@ -30,45 +27,60 @@ def generate():
     number_of_rooms = get_room_amount(stage_id)
     print(number_of_rooms)
     floor = Floor(Globals.height, Globals.width)
-    floor.add_room(random.randint(0, 8), random.randint(0, 7), Color.ORANGE)
+    start_room: tuple = (random.randint(0, 8), random.randint(0, 7))
+    floor.add_room(start_room[0], start_room[1], Color.ORANGE)
     number_of_current_rooms = 1
+    room_queue: deque = deque([])
+    room_queue.append(start_room)
+
     while number_of_current_rooms < number_of_rooms:
-        for room in floor.get_rooms():
-            if room.get_y() - 1 > 0:
-                if not floor.contains_room(room.get_x(), room.get_y() - 1) and (
-                        floor.count_neighbours(room.get_x(), room.get_y() - 1) <= 1):
-                    if random.randint(1, 2) == 2:
-                        floor.add_room(room.get_x(), room.get_y() - 1)
-                        number_of_current_rooms += 1
-                        if number_of_rooms == number_of_current_rooms:
-                            break
 
-            if room.get_y() + 1 < Globals.height:
-                if not floor.contains_room(room.get_x(), room.get_y() + 1) and floor.count_neighbours(room.get_x(),
-                                                                                                      room.get_y() + 1) <= 1:
+        while len(room_queue) > 0 and number_of_current_rooms < number_of_rooms:
+            room = room_queue.pop()
+            if room[1] - 1 > 0:
+                if not floor.contains_room(room[0], room[1] - 1) and (
+                        floor.count_neighbours(room[0], room[1] - 1) <= 1):
                     if random.randint(1, 2) == 2:
-                        floor.add_room(room.get_x(), room.get_y() + 1)
+                        floor.add_room(room[0], room[1] - 1)
+                        room_queue.append((room[0], room[1] - 1))
                         number_of_current_rooms += 1
                         if number_of_rooms == number_of_current_rooms:
                             break
+                    else:
+                        room_queue.appendleft(room)
 
-            if room.get_x() + 1 < Globals.width:
-                if not floor.contains_room(room.get_x() + 1, room.get_y()) and floor.count_neighbours(room.get_x() + 1,
-                                                                                                      room.get_y()) <= 1:
+            if room[1] + 1 < Globals.height:
+                if not floor.contains_room(room[0], room[1] + 1) and floor.count_neighbours(room[0], room[1] + 1) <= 1:
                     if random.randint(1, 2) == 2:
-                        floor.add_room(room.get_x() + 1, room.get_y())
+                        floor.add_room(room[0], room[1] + 1)
+                        room_queue.append((room[0], room[1] + 1))
                         number_of_current_rooms += 1
                         if number_of_rooms == number_of_current_rooms:
                             break
+                    else:
+                        room_queue.appendleft(room)
 
-            if room.get_x() - 1 > 0:
-                if not floor.contains_room(room.get_x() - 1, room.get_y()) and floor.count_neighbours(room.get_x() - 1,
-                                                                                                      room.get_y()) <= 1:
+            if room[0] + 1 < Globals.width:
+                if not floor.contains_room(room[0] + 1, room[1]) and floor.count_neighbours(room[0] + 1,  room[1]) <= 1:
                     if random.randint(1, 2) == 2:
-                        floor.add_room(room.get_x() - 1, room.get_y())
+                        floor.add_room(room[0] + 1, room[1])
+                        room_queue.append((room[0] + 1, room[1]))
                         number_of_current_rooms += 1
                         if number_of_rooms == number_of_current_rooms:
                             break
+                    else:
+                        room_queue.appendleft(room)
+
+            if room[0] - 1 > 0:
+                if not floor.contains_room(room[0] - 1, room[1]) and floor.count_neighbours(room[0] - 1, room[1]) <= 1:
+                    if random.randint(1, 2) == 2:
+                        floor.add_room(room[0] - 1, room[1])
+                        room_queue.append((room[0] - 1, room[1]))
+                        number_of_current_rooms += 1
+                        if number_of_rooms == number_of_current_rooms:
+                            break
+                    else:
+                        room_queue.appendleft(room)
 
 
 def run():
