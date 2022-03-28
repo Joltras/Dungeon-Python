2acf8eaec3e24c029e9b8d20fab3c8e1ecd7a9cc
Joltra <MinnyMouseXDmail.com>
2022-03-28 19:37:47 +0200

    Added width and height

--- Room.py
+++ Room.py
@@ -4,11 +4,13 @@ import Globals
 
 
 class Room:
-    def __init__(self, x: int, y: int, color: Color):
+    def __init__(self, x: int, y: int, color: Color, width=Globals.room_width, height=Globals.room_height):
         self.__x: int = x
         self.__y: int = y
         self.__color = color
-        self.__rect = pygame.Rect(self.__x * Globals.room_width + Globals.floor_plan_coordinates[1], self.__y * Globals.room_height + Globals.floor_plan_coordinates[0], Globals.room_width, Globals.room_height)
+        self.__rect = pygame.Rect(self.__x * Globals.room_width + Globals.floor_plan_coordinates[1],
+                                  self.__y * Globals.room_height + Globals.floor_plan_coordinates[0],
+                                  width, height)
 
     def draw(self, screen):
         pygame.draw.rect(screen, self.__color.value, self.__rect)
@@ -18,3 +20,12 @@ class Room:
 
     def get_y(self):
         return self.__y
+
+    def get_color(self):
+        return self.__color
+
+    def get_rect(self):
+        return self.__rect
+
+
+
