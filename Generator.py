66403c268b51ec9d4200d1ad536f1591af500866
Joltra <MinnyMouseXDmail.com>
2022-03-29 22:45:55 +0200

    Added constants for colors and methods for marking dead ends and adding special rooms

--- Generator.py
+++ Generator.py
@@ -1,12 +1,17 @@
 import math
 import random
 import Globals
-from Globals import Color
+from Globals import Color, RoomType
 from Floor import Floor
 import pygame
 from collections import deque
 
 MAX_ROOMS: int = 15
+NORMAL_ROOM_COLOR = Color.VIOLET
+START_ROOM_COLOR = Color.ORANGE
+BOSS_ROOM_COLOR = Color.RED
+ITEM_ROOM_COLOR = Color.GREEN
+SPECIAL_ROOMS = (RoomType.BOSS_ROOM, RoomType.ITEM_ROOM)
 
 
 class Generator:
@@ -28,7 +33,7 @@ class Generator:
         print(number_of_rooms)
         self.__floor = Floor(Globals.height, Globals.width)
         start_room: tuple = (random.randint(0, 8), random.randint(0, 7))
-        self.__floor.add_room(start_room[0], start_room[1], Color.ORANGE, 5)
+        self.__floor.add_room(start_room[0], start_room[1], START_ROOM_COLOR, RoomType.START_ROOM)
         self.__floor.add_to_floor_grid(start_room[0], start_room[1])
         number_of_current_rooms = 1
         room_queue: deque = deque([])
@@ -39,7 +44,7 @@ class Generator:
             while number_of_current_rooms < number_of_rooms:
                 room = room_queue.pop()
                 room_queue.appendleft(room)
-                if room[1] - 1 > 0:
+                if room[1] - 1 >= 0:
                     if not self.__floor.contains_room(room[0], room[1] - 1) and (
                             self.__floor.count_neighbours(room[0], room[1] - 1) <= 1):
                         if random.randint(1, 2) == 2:
@@ -71,7 +76,7 @@ class Generator:
                             if number_of_rooms == number_of_current_rooms:
                                 break
 
-                if room[0] - 1 > 0:
+                if room[0] - 1 >= 0:
                     if not self.__floor.contains_room(room[0] - 1, room[1]) and self.__floor.count_neighbours(
                             room[0] - 1,
                             room[1]) <= 1:
@@ -84,9 +89,35 @@ class Generator:
         room_queue.remove(start_room)
         while len(room_queue) > 0:
             room = room_queue.pop()
-            self.__floor.add_room(room[0], room[1])
+            self.__floor.add_room(room[0], room[1], NORMAL_ROOM_COLOR)
+
+        dead_ends = self.mark_dead_ends()
+        self.add_special_rooms(dead_ends)
+        self.__floor.add_doors_to_rooms()
 
         print(self.__floor.get_floor())
+
+    def mark_dead_ends(self):
+        dead_end_index = ()
+        i = 0
+        while i < len(self.__floor.get_rooms()):
+            room = self.__floor.get_rooms()[i]
+            if self.__floor.is_dead_end(room.get_x(), room.get_y()):
+                if not room.get_type() == RoomType.START_ROOM:
+                    room.set_type(RoomType.DEAD_END)
+                    dead_end_index += (i,)
+            i += 1
+        return dead_end_index
+
+    def add_special_rooms(self, dead_ends):
+        i = 0
+        while i < len(SPECIAL_ROOMS) and i < len(dead_ends):
+            self.__floor.get_rooms()[dead_ends[i]].set_type = SPECIAL_ROOMS[i]
+            if SPECIAL_ROOMS[i] == RoomType.BOSS_ROOM:
+                self.__floor.get_rooms()[dead_ends[i]].set_color(BOSS_ROOM_COLOR)
+            elif SPECIAL_ROOMS[i] == RoomType.ITEM_ROOM:
+                self.__floor.get_rooms()[dead_ends[i]].set_color(ITEM_ROOM_COLOR)
+            i += 1
 
     def run(self):
         active: bool = True
