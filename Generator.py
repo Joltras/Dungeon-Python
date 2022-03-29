5f5b4eddabe2204ee01c7c876c3a0a3f18aebe86
Joltra <MinnyMouseXDmail.com>
2022-03-29 17:37:07 +0200

    Added Generator class

--- Generator.py
+++ Generator.py
@@ -6,96 +6,98 @@ from Floor import Floor
 import pygame
 from collections import deque
 
-number_of_rooms: int
-max_rooms: int = 15
-stage_id = 2
-
-screen = pygame.display.set_mode((Globals.window_width, Globals.window_height))
-clock = pygame.time.Clock()
-floor: Floor
+MAX_ROOMS: int = 15
 
 
-def get_room_amount(stage_id: int) -> int:
-    if stage_id == -1:
-        stage_id = 1
-    return min(max_rooms, int(random.randint(0, 1) + 5 + math.floor(stage_id * 10) / 3.0))
+class Generator:
+    def __init__(self):
+        self.__clock = pygame.display.set_mode((Globals.window_width, Globals.window_height))
+        self.__floor: Floor = None
+        self.__screen = pygame.display.set_mode((Globals.window_width, Globals.window_height))
+        self.__clock = pygame.time.Clock()
+        self.__stage_id = 2
+        self.__number_of_rooms: int
 
+    def get_room_amount(self) -> int:
+        if self.__stage_id == -1:
+            self.__stage_id = 1
+        return min(MAX_ROOMS, int(random.randint(0, 1) + 5 + math.floor(self.__stage_id * 10) / 3.0))
 
-def generate():
-    global number_of_rooms
-    global floor
-    number_of_rooms = get_room_amount(stage_id)
-    print(number_of_rooms)
-    floor = Floor(Globals.height, Globals.width)
-    start_room: tuple = (random.randint(0, 8), random.randint(0, 7))
-    floor.add_room(start_room[0], start_room[1], Color.ORANGE)
-    floor.add_to_floor_grid(start_room[0], start_room[1])
-    number_of_current_rooms = 1
-    room_queue: deque = deque([])
-    room_queue.append(start_room)
-
-    while number_of_current_rooms < number_of_rooms:
+    def generate(self):
+        number_of_rooms = self.get_room_amount()
+        print(number_of_rooms)
+        self.__floor = Floor(Globals.height, Globals.width)
+        start_room: tuple = (random.randint(0, 8), random.randint(0, 7))
+        self.__floor.add_room(start_room[0], start_room[1], Color.ORANGE, 5)
+        self.__floor.add_to_floor_grid(start_room[0], start_room[1])
+        number_of_current_rooms = 1
+        room_queue: deque = deque([])
+        room_queue.append(start_room)
 
         while number_of_current_rooms < number_of_rooms:
+
+            while number_of_current_rooms < number_of_rooms:
+                room = room_queue.pop()
+                room_queue.appendleft(room)
+                if room[1] - 1 > 0:
+                    if not self.__floor.contains_room(room[0], room[1] - 1) and (
+                            self.__floor.count_neighbours(room[0], room[1] - 1) <= 1):
+                        if random.randint(1, 2) == 2:
+                            room_queue.append((room[0], room[1] - 1))
+                            self.__floor.add_to_floor_grid(room[0], room[1] - 1)
+                            number_of_current_rooms += 1
+                            if number_of_rooms == number_of_current_rooms:
+                                break
+
+                if room[1] + 1 < Globals.height:
+                    if not self.__floor.contains_room(room[0], room[1] + 1) and self.__floor.count_neighbours(room[0],
+                                                                                                              room[
+                                                                                                                  1] + 1) <= 1:
+                        if random.randint(1, 2) == 2:
+                            room_queue.append((room[0], room[1] + 1))
+                            self.__floor.add_to_floor_grid(room[0], room[1] + 1)
+                            number_of_current_rooms += 1
+                            if number_of_rooms == number_of_current_rooms:
+                                break
+
+                if room[0] + 1 < Globals.width:
+                    if not self.__floor.contains_room(room[0] + 1, room[1]) and self.__floor.count_neighbours(
+                            room[0] + 1,
+                            room[1]) <= 1:
+                        if random.randint(1, 2) == 2:
+                            room_queue.append((room[0] + 1, room[1]))
+                            self.__floor.add_to_floor_grid(room[0] + 1, room[1])
+                            number_of_current_rooms += 1
+                            if number_of_rooms == number_of_current_rooms:
+                                break
+
+                if room[0] - 1 > 0:
+                    if not self.__floor.contains_room(room[0] - 1, room[1]) and self.__floor.count_neighbours(
+                            room[0] - 1,
+                            room[1]) <= 1:
+                        if random.randint(1, 2) == 2:
+                            room_queue.append((room[0] - 1, room[1]))
+                            self.__floor.add_to_floor_grid(room[0] - 1, room[1])
+                            number_of_current_rooms += 1
+                            if number_of_rooms == number_of_current_rooms:
+                                break
+        room_queue.remove(start_room)
+        while len(room_queue) > 0:
             room = room_queue.pop()
-            room_queue.appendleft(room)
-            if room[1] - 1 > 0:
-                if not floor.contains_room(room[0], room[1] - 1) and (
-                        floor.count_neighbours(room[0], room[1] - 1) <= 1):
-                    if random.randint(1, 2) == 2:
-                        room_queue.append((room[0], room[1] - 1))
-                        floor.add_to_floor_grid(room[0], room[1] - 1)
-                        number_of_current_rooms += 1
-                        if number_of_rooms == number_of_current_rooms:
-                            break
+            self.__floor.add_room(room[0], room[1])
 
-            if room[1] + 1 < Globals.height:
-                if not floor.contains_room(room[0], room[1] + 1) and floor.count_neighbours(room[0], room[1] + 1) <= 1:
-                    if random.randint(1, 2) == 2:
-                        room_queue.append((room[0], room[1] + 1))
-                        floor.add_to_floor_grid(room[0], room[1] + 1)
-                        number_of_current_rooms += 1
-                        if number_of_rooms == number_of_current_rooms:
-                            break
+        print(self.__floor.get_floor())
 
-            if room[0] + 1 < Globals.width:
-                if not floor.contains_room(room[0] + 1, room[1]) and floor.count_neighbours(room[0] + 1, room[1]) <= 1:
-                    if random.randint(1, 2) == 2:
-                        room_queue.append((room[0] + 1, room[1]))
-                        floor.add_to_floor_grid(room[0] + 1, room[1])
-                        number_of_current_rooms += 1
-                        if number_of_rooms == number_of_current_rooms:
-                            break
-
-            if room[0] - 1 > 0:
-                if not floor.contains_room(room[0] - 1, room[1]) and floor.count_neighbours(room[0] - 1, room[1]) <= 1:
-                    if random.randint(1, 2) == 2:
-                        room_queue.append((room[0] - 1, room[1]))
-                        floor.add_to_floor_grid(room[0] - 1, room[1])
-                        number_of_current_rooms += 1
-                        if number_of_rooms == number_of_current_rooms:
-                            break
-    print(len(room_queue))
-    room_queue.remove(start_room)
-    while len(room_queue) > 0:
-        room = room_queue.pop()
-        floor.add_room(room[0], room[1])
-    print(floor.get_floor())
-
-
-def run():
-    active: bool = True
-    generate()
-    while active:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                active = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                generate()
-        screen.fill(Color.WHITE.value)
-        floor.draw(screen)
-        pygame.display.flip()
-        clock.tick(5)
-
-
-run()
+    def run(self):
+        active: bool = True
+        self.generate()
+        while active:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    active = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.generate()
+            self.__screen.fill(Color.WHITE.value)
+            self.__floor.draw(self.__screen)
+            pygame.display.flip()
+            self.__clock.tick(5)
