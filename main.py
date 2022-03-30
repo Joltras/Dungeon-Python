6e8f406734a700cca30a5f17244b87d3b1eb3492
Joshua <Joshua.Doll@online.de>
2022-03-30 20:58:47 +0200

    Add seed as argument of generator

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from Generator import Generator
+import sys
+import secrets
 
-
-def main():
-    generator = Generator()
+def main(seed):
+    generator = Generator(seed)
     generator.run()
 
 
 if __name__ == '__main__':
-    main()
+    if (len(sys.argv[1:]) < 1):
+        seed = secrets.token_urlsafe(8)
+    else:
+        seed = sys.argv[1]
+    main(seed)
