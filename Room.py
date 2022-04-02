839630c3cd0cdd5739ced2d3b3987b9b1a1c9c24
Joltra <MinnyMouseXDmail.com>
2022-04-02 12:17:15 +0200

    Added getstate and toJSON method

--- Room.py
+++ Room.py
@@ -1,3 +1,5 @@
+import json
+
 import pygame
 from Globals import Color, DoorFace, RoomType
 import Globals
@@ -9,47 +11,69 @@ DOOR_COLOR = Color.BLACK.value
 
 
 class Room:
-    def __init__(self, x: int, y: int, color: Color, room_type, width=Globals.room_width, height=Globals.room_height):
+    def __init__(self, x: int, y: int, color: Color, room_type: RoomType, width=Globals.room_width, height=Globals.room_height):
         self.__x: int = x
         self.__y: int = y
-        self.__room_type = room_type
-        self.__color = color
-        self.__doors = []
-        self.__rect = pygame.Rect(self.__x * Globals.room_width + Globals.floor_plan_coordinates[1],
-                                  self.__y * Globals.room_height + Globals.floor_plan_coordinates[0],
-                                  width, height)
+        self.room_type = room_type
+        self.color = color
+        self.doors = []
+        self.rect = pygame.Rect(self.__x * Globals.room_width + Globals.floor_plan_coordinates[1],
+                                self.__y * Globals.room_height + Globals.floor_plan_coordinates[0],
+                                width, height)
+
+    def __getstate__(self):
+        state = dict(self.__dict__)
+        del state["color"]
+        del state["rect"]
+        doors = []
+        for door in self.doors:
+            if door == DoorFace.UP:
+                doors.append('Up')
+            elif door == DoorFace.RIGHT:
+                doors.append('Right')
+            elif door == DoorFace.DOWN:
+                doors.append('Down')
+            elif door == DoorFace.LEFT:
+                doors.append('Left')
+        state["doors"] = doors
+        state["room_type"] = self.room_type.value
+        print(state)
+        return state
+
+    def toJSON(self):
+        return json.dumps(self.__getstate__(), sort_keys=True, indent=4)
 
     def add_door(self, door_face: DoorFace):
-        if len(self.__doors) < MAX_DOOR_AMOUNT:
-            self.__doors.append(door_face)
+        if len(self.doors) < MAX_DOOR_AMOUNT:
+            self.doors.append(door_face)
 
     def set_type(self, room_type: RoomType):
-        self.__room_type = room_type
+        self.room_type = room_type
 
     def get_type(self):
-        return self.__room_type
+        return self.room_type
 
     def draw(self, screen):
-        pygame.draw.rect(screen, self.__color.value, self.__rect)
+        pygame.draw.rect(screen, self.color.value, self.rect)
 
     def draw_doors(self, screen):
-        for door in self.__doors:
+        for door in self.doors:
             if door == DoorFace.LEFT:
                 pygame.draw.line(screen, DOOR_COLOR,
-                                 (self.__rect.left, self.__rect.top + Globals.room_height / 4),
-                                 (self.__rect.left, self.__rect.top + Globals.room_height / 4 * 3), LINE_THICKNESS)
+                                 (self.rect.left, self.rect.top + Globals.room_height / 4),
+                                 (self.rect.left, self.rect.top + Globals.room_height / 4 * 3), LINE_THICKNESS)
             elif door == DoorFace.RIGHT:
                 pygame.draw.line(screen, DOOR_COLOR,
-                                 (self.__rect.left + Globals.room_width, self.__rect.top + Globals.room_height / 4),
-                                 (self.__rect.left + Globals.room_width, self.__rect.top + Globals.room_height / 4 * 3), LINE_THICKNESS)
+                                 (self.rect.left + Globals.room_width, self.rect.top + Globals.room_height / 4),
+                                 (self.rect.left + Globals.room_width, self.rect.top + Globals.room_height / 4 * 3), LINE_THICKNESS)
             elif door == DoorFace.UP:
                 pygame.draw.line(screen, DOOR_COLOR,
-                                 (self.__rect.left + Globals.room_width / 4, self.__rect.top),
-                                 (self.__rect.left + Globals.room_width / 4 * 3, self.__rect.top), LINE_THICKNESS)
+                                 (self.rect.left + Globals.room_width / 4, self.rect.top),
+                                 (self.rect.left + Globals.room_width / 4 * 3, self.rect.top), LINE_THICKNESS)
             elif door == DoorFace.DOWN:
                 pygame.draw.line(screen, DOOR_COLOR,
-                                 (self.__rect.left + Globals.room_width / 4, self.__rect.top + Globals.room_height),
-                                 (self.__rect.left + Globals.room_width / 4 * 3, self.__rect.top + Globals.room_height), LINE_THICKNESS)
+                                 (self.rect.left + Globals.room_width / 4, self.rect.top + Globals.room_height),
+                                 (self.rect.left + Globals.room_width / 4 * 3, self.rect.top + Globals.room_height), LINE_THICKNESS)
 
     def get_x(self):
         return self.__x
@@ -58,10 +82,10 @@ class Room:
         return self.__y
 
     def get_color(self):
-        return self.__color
+        return self.color
 
     def set_color(self, color: Color):
-        self.__color = color
+        self.color = color
 
     def get_rect(self):
-        return self.__rect
+        return self.rect
