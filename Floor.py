2de3201e1a4675013805c4b21c190ed9c31f17ac
Joltra <MinnyMouseXDmail.com>
2022-04-02 12:12:38 +0200

    Added getstate and toJSON method

--- Floor.py
+++ Floor.py
@@ -1,3 +1,5 @@
+import json
+
 import pygame
 import Globals
 from Globals import Color, RoomType, DoorFace
@@ -11,19 +13,38 @@ class Floor:
         self.__height = height
         self.__width = width
         self.__rooms: list = []
-        self.__floor_grid = np.zeros((height, width))
-        self.__rect = pygame.Rect(Globals.floor_plan_coordinates[1], Globals.floor_plan_coordinates[0],
-                                  width * Globals.room_width, height * Globals.room_height)
+        self.floor_grid = np.zeros((height, width))
+        self.rect = pygame.Rect(Globals.floor_plan_coordinates[1], Globals.floor_plan_coordinates[0],
+                                width * Globals.room_width, height * Globals.room_height)
+
+    def __getstate__(self):
+        state = dict(self.__dict__)
+        del state['rect']
+        del state['floor_grid']
+        return state
+
+    def toJSON(self):
+        currentIndex: int = 0
+        maxIndex: int = len(self.__rooms)
+        j = "["
+        for room in self.__rooms:
+            if currentIndex < maxIndex - 1:
+                j += room.toJSON() + ","
+            else:
+                j += room.toJSON()
+
+            currentIndex += 1
+        return j + "]"
 
     def draw(self, screen):
         for room in self.__rooms:
             room.draw(screen)
         for room in self.__rooms:
             room.draw_doors(screen)
-        pygame.draw.rect(screen, Color.DARK_GRAY.value, self.__rect, 2)
+        pygame.draw.rect(screen, Color.DARK_GRAY.value, self.rect, 2)
 
     def add_to_floor_grid(self, x, y):
-        self.__floor_grid[y][x] = 1
+        self.floor_grid[y][x] = 1
 
     def add_room(self, x, y, color: Color = Color.VIOLET, room_type=RoomType.NORMAL_ROOM):
         self.__rooms.append(Room(x, y, color, room_type))
@@ -32,34 +53,34 @@ class Floor:
         return self.__rooms
 
     def get_floor(self):
-        return self.__floor_grid
+        return self.floor_grid
 
     def contains_room(self, x, y):
-        return self.__floor_grid[y][x] == 1
+        return self.floor_grid[y][x] == 1
 
     def add_doors_to_rooms(self):
         for room in self.__rooms:
             x = room.get_x()
             y = room.get_y()
-            if x + 1 < len(self.__floor_grid[y]) and self.__floor_grid[y][x + 1] == 1:
+            if x + 1 < len(self.floor_grid[y]) and self.floor_grid[y][x + 1] == 1:
                 room.add_door(DoorFace.RIGHT)
-            if x - 1 >= 0 and self.__floor_grid[y][x - 1] == 1:
+            if x - 1 >= 0 and self.floor_grid[y][x - 1] == 1:
                 room.add_door(DoorFace.LEFT)
-            if y - 1 >= 0 and self.__floor_grid[y - 1][x] == 1:
+            if y - 1 >= 0 and self.floor_grid[y - 1][x] == 1:
                 room.add_door(DoorFace.UP)
-            if y + 1 < len(self.__floor_grid) and self.__floor_grid[y + 1][x] == 1:
+            if y + 1 < len(self.floor_grid) and self.floor_grid[y + 1][x] == 1:
                 room.add_door(DoorFace.DOWN)
 
     def count_neighbours(self, x, y):
         neighbours = 0
-        if y + 1 < len(self.__floor_grid):
-            neighbours += self.__floor_grid[y + 1][x]
+        if y + 1 < len(self.floor_grid):
+            neighbours += self.floor_grid[y + 1][x]
         if y - 1 >= 0:
-            neighbours += self.__floor_grid[y - 1][x]
-        if x + 1 < len(self.__floor_grid[y]):
-            neighbours += self.__floor_grid[y][x + 1]
+            neighbours += self.floor_grid[y - 1][x]
+        if x + 1 < len(self.floor_grid[y]):
+            neighbours += self.floor_grid[y][x + 1]
         if x - 1 >= 0:
-            neighbours += self.__floor_grid[y][x - 1]
+            neighbours += self.floor_grid[y][x - 1]
         return neighbours
 
     def is_dead_end(self, x: int, y: int) -> bool:
