c34614b6223bc8c93a588ef94aac3fdfbd59024b
Joltra <MinnyMouseXDmail.com>
2022-09-03 17:57:36 +0200

    Change names of variables

--- src/Floor.py
+++ src/Floor.py
@@ -13,19 +13,17 @@ class Floor:
         :param height: height of the floor
         :param width: width of the floor
         """
-        self.__height = height
-        self.__width = width
-        self.__rooms: list = []
-        self.floor_grid = np.zeros((height, width))
-        self.rect = pygame.Rect(Globals.floor_plan_coordinates[1], Globals.floor_plan_coordinates[0],
-                                width * Globals.room_width, height * Globals.room_height)
-        self.room_id = 0
+        self._rooms: list = []
+        self._floor_grid = np.zeros((height, width))
+        self._rect = pygame.Rect(Globals.floor_plan_coordinates[1], Globals.floor_plan_coordinates[0],
+                                 width * Globals.room_width, height * Globals.room_height)
+        self._room_id = 0
 
     def __getstate__(self):
         state = dict(self.__dict__)
-        del state['rect']
-        del state['floor_grid']
-        del state['room_id']
+        del state['_rect']
+        del state['_floor_grid']
+        del state['_room_id']
         return state
 
     def toJSON(self):
@@ -34,27 +32,27 @@ class Floor:
         :return: json string
         """
         current_index: int = 0
-        max_index: int = len(self.__rooms)
-        j = "["
-        for room in self.__rooms:
+        max_index: int = len(self._rooms)
+        json_string = "{\n\"rooms\": ["
+        for room in self._rooms:
             if current_index < max_index - 1:
-                j += room.toJSON() + ","
+                json_string += room.toJSON() + ","
             else:
-                j += room.toJSON()
+                json_string += room.toJSON()
 
             current_index += 1
-        return j + "]"
+        return json_string + "]\n}"
 
     def draw(self, screen: pygame.Surface) -> None:
         """
         Draws the floor with all the rooms on the screen.
         :param screen: screen to draw on
         """
-        for room in self.__rooms:
+        for room in self._rooms:
             room.draw(screen)
-        for room in self.__rooms:
+        for room in self._rooms:
             room.draw_doors(screen)
-        pygame.draw.rect(screen, Color.DARK_GRAY.value, self.rect, 2)
+        pygame.draw.rect(screen, Color.DARK_GRAY.value, self._rect, 2)
 
     def add_to_floor_grid(self, x: int, y: int) -> None:
         """
@@ -63,7 +61,7 @@ class Floor:
         :param y: y coordinate of the room
         :return:
         """
-        self.floor_grid[y][x] = 1
+        self._floor_grid[y][x] = 1
 
     def add_room(self, x: int, y: int, color: Color = Color.VIOLET, room_type=RoomType.NORMAL_ROOM):
         """
@@ -73,8 +71,8 @@ class Floor:
         :param color: color of the room (default = violet)
         :param room_type: type of the room (default = normal room)
         """
-        self.__rooms.append(Room(x=x, y=y, color=color, room_type=room_type, room_id=self.room_id))
-        self.room_id += 1
+        self._rooms.append(Room(x=x, y=y, color=color, room_type=room_type, room_id=self._room_id))
+        self._room_id += 1
 
     def add_room_next_to(self, room: Room, direction: Directions, color: Color, room_type: RoomType) -> None:
         """
@@ -107,24 +105,24 @@ class Floor:
         :param room: Room which is connected to the teleport room.
         :param color: color for the room (default = gray)
         """
-        t_room = TeleportRoom(x=room.get_x(), y=room.get_y(), color=color, room_id=self.room_id,
+        t_room = TeleportRoom(x=room.get_x(), y=room.get_y(), color=color, room_id=self._room_id,
                               room_type=RoomType.BOSS_TELEPORT_ROOM, teleport_room_id=room.get_id())
-        self.__rooms.append(t_room)
-        self.room_id += 1
+        self._rooms.append(t_room)
+        self._room_id += 1
 
     def get_rooms(self) -> list:
         """
         Returns a list containing all the rooms of the floor.
         :return: rooms
         """
-        return self.__rooms
+        return self._rooms
 
     def get_floor_grid(self) -> np.ndarray:
         """
         Returns the floor grid of the floor.
         :return: floor grid
         """
-        return self.floor_grid
+        return self._floor_grid
 
     def contains_room(self, x: int, y: int) -> bool:
         """
@@ -133,22 +131,22 @@ class Floor:
         :param y: y coordinate
         :return: true if a there is a room otherwise false
         """
-        return self.floor_grid[y][x] == 1
+        return self._floor_grid[y][x] == 1
 
     def add_doors_to_rooms(self) -> None:
         """
         Adds doors to all rooms on the floor.
         """
-        for room in self.__rooms:
+        for room in self._rooms:
             x = room.get_x()
             y = room.get_y()
-            if x + 1 < len(self.floor_grid[y]) and self.floor_grid[y][x + 1] == 1:
+            if x + 1 < len(self._floor_grid[y]) and self._floor_grid[y][x + 1] == 1:
                 room.add_door(DoorFace.EAST)
-            if x - 1 >= 0 and self.floor_grid[y][x - 1] == 1:
+            if x - 1 >= 0 and self._floor_grid[y][x - 1] == 1:
                 room.add_door(DoorFace.WEST)
-            if y - 1 >= 0 and self.floor_grid[y - 1][x] == 1:
+            if y - 1 >= 0 and self._floor_grid[y - 1][x] == 1:
                 room.add_door(DoorFace.TOP)
-            if y + 1 < len(self.floor_grid) and self.floor_grid[y + 1][x] == 1:
+            if y + 1 < len(self._floor_grid) and self._floor_grid[y + 1][x] == 1:
                 room.add_door(DoorFace.BOTTOM)
 
     def count_neighbours(self, x: int, y: int) -> int:
@@ -159,14 +157,14 @@ class Floor:
         :return: number of neighbour rooms
         """
         neighbours = 0
-        if y + 1 < len(self.floor_grid):
-            neighbours += self.floor_grid[y + 1][x]
+        if y + 1 < len(self._floor_grid):
+            neighbours += self._floor_grid[y + 1][x]
         if y - 1 >= 0:
-            neighbours += self.floor_grid[y - 1][x]
-        if x + 1 < len(self.floor_grid[y]):
-            neighbours += self.floor_grid[y][x + 1]
+            neighbours += self._floor_grid[y - 1][x]
+        if x + 1 < len(self._floor_grid[y]):
+            neighbours += self._floor_grid[y][x + 1]
         if x - 1 >= 0:
-            neighbours += self.floor_grid[y][x - 1]
+            neighbours += self._floor_grid[y][x - 1]
         return neighbours
 
     def is_dead_end(self, x: int, y: int) -> bool:
