7d6ea7d00f46ccc65b5b96d653f918d547410eb4
Joltra <MinnyMouseXDmail.com>
2022-09-03 17:56:47 +0200

    Add test class for generator

---
+++ test/Generator_Test.py
@@ -0,0 +1,15 @@
+import unittest
+
+from Generator import Generator
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self._generator = Generator("1", "test", False)
+
+    def test_to_json(self):
+        expected = ""
+        self.assertEqual(expected, self._generator.toJSON())
+
+if __name__ == '__main__':
+    unittest.main()
