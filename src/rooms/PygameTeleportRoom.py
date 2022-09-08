14e70bc8e17f6433f38e1cb19a5521984a399d92
Joltra <MinnyMouseXDmail.com>
2022-09-08 16:16:54 +0200

    Change Name from TeleportRoom to PygameTeleportRoom

---
+++ src/rooms/PygameTeleportRoom.py
@@ -0,0 +1,12 @@
+import Globals
+from Globals import RoomType, Color
+from rooms.PygameNormalRoom import PygameNormalRoom
+
+
+class PygameTeleportRoom(PygameNormalRoom):
+
+    def __init__(self, x: int, y: int, room_id: int, teleport_room_id:int, color: Color = Color.GRAY,
+                 room_type: RoomType = RoomType.BOSS_TELEPORT_ROOM,
+                 width=Globals.room_width,
+                 height=Globals.room_height):
+        super().__init__(x, y, color, room_id, room_type, width, height)
