3b3efb23dd806c9423d12963f2e9454574ef6c0a
Joltra <MinnyMouseXDmail.com>
2022-09-23 14:13:25 +0200

    Add utils

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import random
+import math
+import Globals
+
+
+def calculate_room_amount(stage_id: int):
+    """
+    Calculates the room amount.
+    :return: room amount
+    """
+    return min(Globals.MAX_ROOMS, int(random.randint(0, 1) + 5 + math.floor(stage_id * 10) / 3.0))
+
+
+def place_room() -> bool:
+    """
+    Checks if a new room should be placed.
+    :return: True when room should be placed otherwise False
+    """
+    return random.randint(1, 2) == 2
