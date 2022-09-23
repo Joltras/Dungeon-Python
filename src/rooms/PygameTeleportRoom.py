846433b4a679b06ac1e6c291942925777b8b0eeb
Joltra <MinnyMouseXDmail.com>
2022-09-23 15:52:05 +0200

    Rename const values

--- src/rooms/PygameTeleportRoom.py
+++ src/rooms/PygameTeleportRoom.py
@@ -7,7 +7,7 @@ class PygameTeleportRoom(PygameNormalRoom):
 
     def __init__(self, x: int, y: int, room_id: int, teleport_room_id: int,
                  room_type: RoomType = RoomType.BOSS_TELEPORT_ROOM,
-                 width=Globals.room_width,
-                 height=Globals.room_height):
+                 width=Globals.ROOM_WIDTH,
+                 height=Globals.ROOM_HEIGHT):
         super().__init__(x, y, room_id, room_type, width, height)
         self._teleport_room_id = teleport_room_id
