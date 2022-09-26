88ce3a32e8a8e1aeea7a0733cff151100de83b9f
Joltra <MinnyMouseXDmail.com>
2022-09-26 13:44:53 +0200

    Add test class for the teleport room

---
+++ test/TeleportRoom_Test.py
@@ -0,0 +1,15 @@
+import unittest
+from rooms.TeleportRoom import TeleportRoom
+
+
+class TeleportRoomTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self._t_room = TeleportRoom(1, 2, 3, 4)
+
+    def test_get_connected_room(self):
+        self.assertEqual(4, self._t_room.get_connected_room_id())
+
+
+if __name__ == '__main__':
+    unittest.main()
