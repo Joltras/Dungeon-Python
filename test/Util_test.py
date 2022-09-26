33e9d420c366b5196f2f2fb3b40d4ae20a47e3cc
Joltra <MinnyMouseXDmail.com>
2022-09-26 13:10:14 +0200

    Add test methods for the utils methods

---
+++ test/Util_test.py
@@ -0,0 +1,15 @@
+import unittest
+import Globals
+import utils
+
+class MyTestCase(unittest.TestCase):
+
+    def test_calculate_room_amount(self):
+        rooms = utils.calculate_room_amount(2)
+        self.assertTrue(0 < rooms <= Globals.MAX_ROOMS)
+
+    def test_place_room(self):
+        utils.place_room()
+
+if __name__ == '__main__':
+    unittest.main()
