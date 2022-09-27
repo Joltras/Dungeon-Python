3144c5a68f972e2781390abd28db0872c3a47f6e
Joltra <MinnyMouseXDmail.com>
2022-09-27 14:09:06 +0200

    Add rest controller

---
+++ src/controller.py
@@ -0,0 +1,23 @@
+import os
+import secrets
+from fastapi import FastAPI
+from starlette.responses import FileResponse
+
+import Globals
+from generators.Generator import Generator
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Test"}
+
+
+@app.get("/g")
+async def generate():
+    generator = Generator(secrets.token_hex(16), "floor.json")
+    generator.generate()
+    generator.save()
+    path = os.path.join(Globals.APPLICATION_PATH, "generation/floor.json")
+    return FileResponse(path=path, filename="floor.json")
