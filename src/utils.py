c392fe6d6367cb5dcbd63b62012b54c63199392c
Joltra <MinnyMouseXD@gmail.com>
2023-08-28 20:55:08 +0200

    Add utils method for calculating a point with a starting point and a direction

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import random
 import math
 import globals
+from globals import Direction
 
 
 def calculate_room_amount(stage_id: int):
@@ -17,3 +18,20 @@ def place_room() -> bool:
     :return: True when room should be placed otherwise False
     """
     return random.randint(1, 50) >= 25
+
+
+def add_direction_to_coordinates(direction: Direction, coordinates: tuple) -> tuple:
+    """
+    Calculates the room according to the given room and direction
+    @param direction: direction for the room to calculate
+    @param coordinates: start point for the calculation
+    @return: calculated point
+    """
+    if direction is Direction.UP:
+        return coordinates[0], coordinates[1] - 1
+    if direction is Direction.DOWN:
+        return coordinates[0], coordinates[1] + 1
+    if direction is Direction.LEFT:
+        return coordinates[0] - 1, coordinates[1]
+    if direction is Direction.RIGHT:
+        return coordinates[0] + 1, coordinates[1]
