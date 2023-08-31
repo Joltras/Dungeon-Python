9f51951c5d0de2ef2fe330e14658970ee75428e3
Joltra <MinnyMouseXD@gmail.com>
2023-08-31 16:23:50 +0200

    Add an endpoint that takes an id for the generation of the floor

--- src/controller.py
+++ src/controller.py
@@ -14,13 +14,26 @@ async def root():
     return {"message": "Test"}
 
 
+@app.get("/g/{floor_id}")
+async def generate_with_id(floor_id: int) -> FileResponse:
+    """
+    This endpoint generates a floor with the given id and returns it in a FileResponse as a json file.
+    @param floor_id id for the floor
+    @return: Floor in json file
+    """
+    generator = Generator(secrets.token_hex(16), "floor.json", floor_id)
+    generator.generate()
+    path = generator.save()
+    return FileResponse(path=path, filename="floor.json")
+
+
 @app.get("/g")
-async def generate():
+async def generate() -> FileResponse:
     """
-    This endpoint generates a floor and returns it in a FileResponse as a json file.
-    @return: Floor in json format
+    This endpoint generates a floor with the id zero and returns it as a json file.
+    @return: Floor as json file
     """
-    generator = Generator(secrets.token_hex(16), "floor.json")
+    generator = Generator(secrets.token_hex(16), "floor.json", 0)
     generator.generate()
     path = generator.save()
     return FileResponse(path=path, filename="floor.json")
