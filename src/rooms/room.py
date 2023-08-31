3138de675babd837af52105a7eac3d4a006f4295
Joltra <MinnyMouseXD@gmail.com>
2023-08-31 12:38:59 +0200

    Update documentation, remove unused equals method

--- src/rooms/room.py
+++ src/rooms/room.py
@@ -5,6 +5,13 @@ from globals import RoomType, DoorFace, MAX_DOOR_AMOUNT
 class Room:
 
     def __init__(self, x: int, y: int, room_id: int, type: RoomType):
+        """
+        Creates a new room with the given values.
+        @param x: x coordinate of the room
+        @param y: y coordinate of the room
+        @param room_id: id of the room
+        @param type: type of the room
+        """
         self._x: int = x
         self._y: int = y
         self._id: int = room_id
@@ -12,6 +19,12 @@ class Room:
         self._doors = []
 
     def __getitem__(self, index: int) -> int:
+        """
+        Provides an easier method to access the x and y coordinate of the room.
+        If the index is not 0 or 1 the method raises an exception.
+        @param index: 0 for x 1 for y
+        @return: Returns x if index equals 0 and y if index equals 1
+        """
         if index < 0 or index > 1:
             raise ValueError(str(index) + "is not a valid index")
         if index == 0:
@@ -19,17 +32,10 @@ class Room:
         if index == 1:
             return self._y
 
-    def __eq__(self, other):
-        are_equal = False
-        if isinstance(other, Room):
-            if other._x == self._x and other._y == self._y and other._type == self._type and self._id == other._id:
-                are_equal = True
-        return are_equal
-
     def to_json(self, indent: int) -> str:
         """
         Creates a json representation of the room object.
-        :return: json string
+        @return: json string
         """
         indent_s: str = globals.BASE_INDENT * indent
         json_string: str = str(globals.BASE_INDENT * (indent - 1)) + "{\n" + indent_s + '"_doors": [\n'
@@ -48,43 +54,43 @@ class Room:
     def set_type(self, room_type: RoomType) -> None:
         """
         Sets the type of the room.
-        :param room_type: type for the room
+        @param room_type: type for the room
         """
         self._type = room_type
 
     def get_type(self) -> RoomType:
         """
         Returns the type of the room.
-        :return: type
+        @return: type
         """
         return self._type
 
     def get_x(self) -> int:
         """
         Returns the x coordinate of the room.
-        :return: x coordinate
+        @return: x coordinate
         """
         return self._x
 
     def get_y(self) -> int:
         """
         Returns the y coordinate of the room.
-        :return: y coordinate
+        @return: y coordinate
         """
         return self._y
 
     def get_id(self) -> int:
         """
         Returns the id of a room.
-        :return: id
+        @return: id
         """
         return self._id
 
     def set_cord(self, x: int, y: int) -> None:
         """
         Sets the coordinates of a room.
-        :param x: new x coordinate
-        :param y: new y coordinate
+        @param x: new x coordinate
+        @param y: new y coordinate
         """
         self._x = x
         self._y = y
@@ -92,14 +98,14 @@ class Room:
     def get_doors(self) -> list:
         """
         Returns a list of all doors
-        :return: list of doors
+        @return: list of doors
         """
         return self._doors
 
     def add_door(self, door_face: DoorFace) -> None:
         """
         Adds a door at the given location to the room.
-        :param door_face: position to place the door
+        @param door_face: position to place the door
         """
         if len(self._doors) < MAX_DOOR_AMOUNT:
             self._doors.append(door_face)
