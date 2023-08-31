29f770a715b07e5bc47d31d4ed1c6cdbc677a441
Joltra <MinnyMouseXD@gmail.com>
2023-08-31 12:38:35 +0200

    Update documentation, remove room type argument from the constructor

--- src/rooms/pygame_teleport_room.py
+++ src/rooms/pygame_teleport_room.py
@@ -7,8 +7,16 @@ from rooms.teleport_room import TeleportRoom
 class PygameTeleportRoom(PygameNormalRoom, TeleportRoom):
 
     def __init__(self, x: int, y: int, room_id: int, teleport_room_id: int,
-                 room_type: RoomType = RoomType.TELEPORT_ROOM,
                  width=globals.ROOM_WIDTH,
                  height=globals.ROOM_HEIGHT):
-        super().__init__(x, y, room_id, room_type, width, height)
+        """
+        Creates a new pygame teleport room with the given values
+        @param x: x coordinate of the room
+        @param y: y coordinate of the room
+        @param room_id: id of the linked room
+        @param teleport_room_id: id of the teleport room
+        @param width: width of the room
+        @param height: height of the room
+        """
+        super().__init__(x, y, room_id, RoomType.TELEPORT_ROOM, width, height)
         self._teleport_room_id = teleport_room_id
