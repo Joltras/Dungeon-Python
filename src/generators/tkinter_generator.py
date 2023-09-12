b0e08ae7d095547eacd2c17583e0e74a70e37ab6
Joltra <MinnyMouseXD@gmail.com>
2023-09-12 14:46:08 +0200

    Add buttons for generating new floors and switching between the existing

--- src/generators/tkinter_generator.py
+++ src/generators/tkinter_generator.py
@@ -1,6 +1,8 @@
 import tkinter as tk
+from  tkinter import ttk
 from collections import deque
 import globals
+import utils
 from floors.tkinter_floor import TkinterFloor
 from generators.generator import Generator
 
@@ -16,18 +18,38 @@ class TkinterGenerator(Generator):
         self._floors = deque()
         self._current_floor = -1
         self._tk = tk.Tk()
+        color = globals.Color.GRAY.value
+        hex = utils.rgb2hex(color[0], color[1], color[2])
+        self._canvas = tk.Canvas(height=(globals.FLOOR_HEIGHT + 1) * globals.ROOM_HEIGHT, width=(globals.ROOM_WIDTH + 1) * globals.ROOM_WIDTH,
+                                 background=hex)
 
     def _create_floor(self) -> None:
-        self._floors.append(TkinterFloor(globals.FLOOR_HEIGHT * globals.ROOM_HEIGHT, globals.FLOOR_WIDTH * globals.ROOM_WIDTH))
+        self._floors.append(TkinterFloor(globals.FLOOR_HEIGHT, globals.FLOOR_WIDTH, canvas=self._canvas))
         self._current_floor = len(self._floors) - 1
         self._floor = self._floors[self._current_floor]
+        self._floors[self._current_floor].draw(tk)
+
+    def _decrease_floor(self):
+        if self._current_floor > 0:
+            self._current_floor -= 1
+            self._floors[self._current_floor].draw(tk)
+
+    def _increase_floor(self):
+        if self._current_floor < len(self._floors) - 1:
+            self._current_floor += 1
+            self._floors[self._current_floor].draw(tk)
 
     def run(self):
         self._tk.title("Dungeon Generator")
-        self._tk.geometry("1000x500")
-        tk.Label(self._tk, text="Current Floor").pack()
+        self._tk.geometry("1200x700")
+        ttk.Label(self._tk, text="Current Floor").pack()
         self.generate()
         self._floors[self._current_floor].draw(tk)
-        button_pre = tk.Button(text="<-")
-        button_pre.pack(side=tk.LEFT)
+        self._canvas.pack(anchor=tk.CENTER, expand=True)
+        button_pre = ttk.Button(text="<-", command=self._decrease_floor)
+        button_pre.pack(side=tk.BOTTOM)
+        button_gen = ttk.Button(text="Generate", command=self.generate)
+        button_gen.pack()
+        button_next = ttk.Button(text="->", command=self._increase_floor)
+        button_next.pack()
         self._tk.mainloop()
