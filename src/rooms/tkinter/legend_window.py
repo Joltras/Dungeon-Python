818fc8f51e879223c2044f8fefb1889258e56a15
Joltras <MinnyMouseXD@gmail.com>
2024-03-29 23:19:24 +0100

    Add file for the legend window

---
+++ src/rooms/tkinter/legend_window.py
@@ -0,0 +1,29 @@
+"""
+This module contains the LegendWindow class which is used to display the legend of room types in the GUI.
+"""
+
+import tkinter as tk
+
+import src.utils.globals as my_globals
+from utils import util_functions
+from utils.room_type import RoomType, Room_Colors, Room_Names
+
+
+def display_legend():
+    """
+    Displays the legend on the canvas.
+    """
+    legend_window = tk.Toplevel()
+    legend_window.title("Room Type Legend")
+    legend_canvas = tk.Canvas(legend_window)
+    legend_canvas.pack()
+    y_offset = 10
+    room_type: RoomType
+    for room_type in RoomType.get_all():
+        color: my_globals.Color = Room_Colors[room_type]
+        legend_canvas.create_rectangle(10, y_offset, 30, y_offset + 20, fill=util_functions.rgb2hex(*color.value))
+        legend_canvas.create_text(40, y_offset + 10, anchor=tk.W, text=Room_Names[room_type])
+        y_offset += 30
+
+
+
